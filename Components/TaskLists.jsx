a91cc3ff1f0c4cb62eb44147c1c092d73eb0d912
michalLenczowski <michalek118@gmail.com>
2017-01-30 15:37:15 +0100

    Update TaskLists.jsx

--- Components/TaskLists.jsx
+++ Components/TaskLists.jsx
@@ -1,12 +1,19 @@
 var Home=function(props){
 	var data=props.data;
 	return (
-		<div className="col-md-10 text-center">
-            <ul className="list-unstyled link-list">
-                {AppState.state.home_list.map((index,i)=>
-                	<li ><span className="index">{i+1}.  </span>{index}
-                <span  className="glyphicon glyphicon-trash" onClick={(index)=>actions.removeList(index)}></span><br/></li>)}<br/>
-               </ul>
+		<div className="col-md-10">
+            <table >
+              <thead></thead>
+                <tbody>
+                  {AppState.state.home_list.map((index,i)=>
+                	<tr >
+                    <td className="index">{i+1}.  </td>
+                    <td className="name">{index.name}</td>
+                    <td className="date">{index.date}</td>
+                    <td id="actions" className="glyphicon glyphicon-trash" onClick={(index)=>actions.removeList(index)}></td>
+                </tr>)}
+               </tbody>
+              </table>  
           </div>
 	)
 }
@@ -14,12 +21,19 @@ var Home=function(props){
 var Work=function(props){
 	var data=props.data;
 	return	(
-		<div className="col-md-10 text-center">
-            <ul className="list-unstyled link-list">
-                {AppState.state.work_list.map((index,i)=>
-                	<li ><span className="index">{i+1}.  </span>{index}
-                <span  className="glyphicon glyphicon-trash" onClick={(index)=>actions.removeList(index)}></span><br/></li>)}<br/>
-               </ul>
+		<div className="col-md-10">
+            <table >
+              <thead></thead>
+                <tbody>
+                  {AppState.state.work_list.map((index,i)=>
+                  <tr >
+                    <td className="index">{i+1}.  </td>
+                    <td className="name">{index.name}</td>
+                    <td className="date">{index.date}</td>
+                    <td id="actions" className="glyphicon glyphicon-trash" onClick={(index)=>actions.removeList(index)}></td>
+                </tr>)}
+               </tbody>
+              </table>  
           </div>
 
 
@@ -29,12 +43,19 @@ var Work=function(props){
 var Other=function(props){
 	var data=props.data;
 	return (
-		<div className="col-md-10 text-center">
-            <ul className="list-unstyled link-list">
-                {AppState.state.other_list.map((index,i)=>
-                	<li ><span className="index">{i+1}.  </span>{index}
-                <span  className="glyphicon glyphicon-trash" onClick={(index)=>actions.removeList(index)}></span><br/></li>)}<br/>
-               </ul>
+		<div className="col-md-10">
+            <table >
+              <thead></thead>
+                <tbody>
+                  {AppState.state.other_list.map((index,i)=>
+                  <tr >
+                    <td className="index">{i+1}.  </td>
+                    <td className="name">{index.name}</td>
+                    <td className="date">{index.date}</td>
+                    <td id="actions" className="glyphicon glyphicon-trash" onClick={(index)=>actions.removeList(index)}></td>
+                </tr>)}
+               </tbody>
+              </table>  
           </div>
 	)
 }
